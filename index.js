d5b1fa3c5ca0ed847cb55edb4ba7d76e4073275a
MAAZ ZAMA <49765187+maaaz15@users.noreply.github.com>
2023-10-26 20:57:42 +0530

    Rebased

--- index.js
+++
@@ -1,9 +0,0 @@
-
-    setInterval(() => {
-        let hrs = (new Date().getHours() <10? "0": "") + new Date().getHours();
-        let mins = (new Date().getMinutes() <10? "0": "") + new Date().getMinutes();
-        let sec = (new Date().getSeconds() <10? "0": "") + new Date().getSeconds();
-        document.getElementById("hrs").innerHTML = hrs;
-        document.getElementById("mins").innerHTML = mins;
-        document.getElementById("secs").innerHTML = sec;
-    }, 1000);
